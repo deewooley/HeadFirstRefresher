9ac63aa2fabb964c836aceb2beae98d8b7845c4b
deewooley <55896326+deewooley@users.noreply.github.com>
2020-03-17 16:05:20 -0400

    first commit

---
+++ encapsulating/GoodDogTestDrive.java
@@ -0,0 +1,13 @@
+package guessingGame.encapsulating;
+
+public class GoodDogTestDrive {
+    public static void main(String[] args) {
+        GoodDog amour = new GoodDog();
+
+
+        amour.setSize(60);
+
+        amour.bark();
+
+    }
+}
